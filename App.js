4f0b6ae4bc92fe14345dff5141861e3315d6f2a0
duong122 <131834049+duong122@users.noreply.github.com>
2023-10-15 20:52:30 +0700

    f

--- App.js
+++ App.js
@@ -1,18 +1,112 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home_1 from './screens/Home_1'
+import Home_1 from './screens/Home_1';
+import Categories from './screens/Categories';
+import Saved from './screens/Saved';
+import Search from './screens/Search';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from "@expo/vector-icons"; 
+import { ScreenStackHeaderLeftView } from 'react-native-screens';
+import { GestureHandlerRootView, TouchableOpacity } from 'react-native-gesture-handler';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStack = createNativeStackNavigator();
+
+const ImageLeft = () => {
+  return (
+    <TouchableOpacity>
+      <Image source={require('./assets/img/fezbot2000-365718-unsplash.png')} style={{borderRadius: 25,}}/>
+    </TouchableOpacity>
+  );
+}
+
+const RightIcon = () => {
+  return (
+    <View style={styles.IconHeaderContainer}>
+      <Image source={require('./assets/img/addNewBtn.png')}/>
+      <Image source={require('./assets/img')}/>
+    </View>
+  );
+}
+
+function HomeStackScreen() {
+  return(
+    <HomeStack.Navigator>
+      <HomeStack.Screen name='Home_1' component={Home_1} options={{
+        headerTitleAlign: 'center',
+        headerTitle: 'Home',
+        headerLeft: () => <ImageLeft/>
+      }}/>
+    </HomeStack.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <NavigationContainer >
-      <Stack.Navigator initialRouteName='Home_1'>
-        <Stack.Screen name='Home_1' component={Home_1}/>
-      </Stack.Navigator>
+    <GestureHandlerRootView style={{flex: 1,}}>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={HomeStackScreen} options={{
+          headerShown: false,
+          tabBarShowLabel: true,         
+          tabBarIcon: (() => {
+            return (
+              <Ionicons 
+              name='md-home'
+              size={24}
+              color={'#20C065'}
+              />
+            );
+            screenOption
+          }),
+          
+        }}></Tab.Screen>
+
+      <Tab.Screen name='Categories' component={Categories} options={{
+          tabBarLabel: 'Categories',      
+          tabBarIcon: (() => {
+            return(
+              <Ionicons
+              name='md-menu'
+              size={24}
+              color={'#20C065'}
+              />
+            );
+           
+          })  
+        }}></Tab.Screen>
+
+        <Tab.Screen name='Saved' component={Saved} options={{
+          tabBarLabel: 'Saved',      
+          tabBarIcon: (() => {
+            return (
+              <Ionicons 
+              name='md-heart'
+              size={24}
+              color={'#20C065'}
+              />
+            );
+          })
+        }}></Tab.Screen>
+        
+        <Tab.Screen name='Search' component={Search} options={{
+          tabBarLabel: 'Search',    
+          tabBarIcon: (() => {
+            return (
+              <Ionicons 
+              name='md-search'
+              size={24}
+              color={'#20C065'}
+              />
+            );
+          })    
+        }}></Tab.Screen>
+      </Tab.Navigator>
     </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 
